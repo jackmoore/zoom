270404468d5a9e06567e2e4521e394e3110a5715
Jack Moore <jack@colorpowered.com>
2011-11-12 03:24:41 -0500

    first commit

---
+++ jquery.zoom.js
@@ -0,0 +1,91 @@
+// Zoom 1.0 - jQuery image zooming plugin
+// (c) 2011 Jack Moore - jacklmoore.com
+// license: www.opensource.org/licenses/mit-license.php
+
+(function ($) {
+    var defaults = {
+        url: false,
+        icon: true,
+        duration: 200
+    };
+
+    $.fn.zoom = function (options) {
+        return this.each(function () {
+            var 
+            root = this,
+            $root = $(root),
+            img = new Image(),
+            $img = $(img),
+            $icon,
+            position = $root.css('position'),
+            settings = $.extend({}, defaults, options || {});
+
+            $root.css({
+                position: /(absolute|fixed)/.test(position) ? position : 'relative',
+                overflow: 'hidden'
+            });
+
+            if (!settings.url) {
+                settings.url = $root.find('img:first')[0].src;
+                if (!settings.url) {
+                    return;
+                }
+            }
+
+            if (settings.icon) {
+                $icon = $('<div class="zoomIcon">').appendTo($root);
+            }
+
+            img.onload = function () {
+                var
+                outerWidth,
+                outerHeight,
+                xRatio,
+                yRatio;
+
+                function ratio() {
+                    outerWidth = $root.outerWidth();
+                    outerHeight = $root.outerHeight();
+                    xRatio = -(img.width - outerWidth) / outerWidth;
+                    yRatio = -(img.height - outerHeight) / outerHeight;
+                }
+
+                ratio();
+
+                $img
+                .addClass('zoomImg')
+                .css({
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    opacity: 0,
+                    width: img.width,
+                    height: img.height,
+                    border: 'none'
+                })
+                .hover(
+                    function () {
+                        ratio();
+
+                        // Skip the fade-in for IE8 and lower since it chokes on fading-in
+                        // and changing position based on mousemovement at the same time.
+                        $img.stop().fadeTo($.support.opacity ? settings.duration : 0, 1);
+
+                    }, 
+                    function () {
+                        $img.stop().fadeTo(settings.duration, 0);
+                    }
+                )
+                .mousemove(function (e) {
+                    img.style.left = (e.pageX - root.offsetLeft) * xRatio + 'px';
+                    img.style.top = (e.pageY - root.offsetTop) * yRatio + 'px';
+                })
+                .appendTo($root);
+            };
+
+            img.src = settings.url;
+        });
+    };
+
+    $.fn.zoom.defaults = defaults;
+}(jQuery));
---
+++ readme.md
@@ -0,0 +1,9 @@
+## Zoom
+
+A small jQuery plugin for zooming images on mouseover.
+
+### Online Demo:
+http://jacklmoore.com/zoom/
+
+### License: 
+http://www.opensource.org/licenses/mit-license.php
