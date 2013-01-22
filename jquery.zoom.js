f5af923f52a86a2274022287ab59be154ed6f89c
jackmoore <jack@colorpowered.com>
2013-01-22 04:34:46 -0500

    Separated event handling from positioning logic to allow easier customization.

--- jquery.zoom.js
+++ jquery.zoom.js
@@ -1,5 +1,5 @@
-// Zoom 1.4 - jQuery image zooming plugin
-// (c) 2012 Jack Moore - jacklmoore.com
+// Zoom 1.6.0 - jQuery image zooming plugin
+// (c) 2013 Jack Moore - jacklmoore.com
 // license: www.opensource.org/licenses/mit-license.php
 
 (function ($) {
@@ -12,84 +12,87 @@
 		on: 'mouseover' // other options: 'grab' or 'click'
 	};
 
+	// Core Zoom Logic, independent of event listeners.
+	$.zoom = function(target, source, img) {
+		var outerWidth,
+			outerHeight,
+			xRatio,
+			yRatio,
+			offset,
+			position = $(target).css('position');
+
+		// The parent element needs positioning so that the zoomed element can be correctly positioned within.
+		$(target).css({
+			position: /(absolute|fixed)/.test() ? position : 'relative',
+			overflow: 'hidden'
+		});
+
+		$(img)
+			.addClass('zoomImg')
+			.css({
+				position: 'absolute',
+				top: 0,
+				left: 0,
+				opacity: 0,
+				width: img.width,
+				height: img.height,
+				border: 'none',
+				maxWidth: 'none'
+			})
+			.appendTo(target);
+
+		return {
+			init: function() {
+				outerWidth = $(target).outerWidth();
+				outerHeight = $(target).outerHeight();
+				xRatio = (img.width - outerWidth) / $(source).outerWidth();
+				yRatio = (img.height - outerHeight) / $(source).outerHeight();
+				offset = $(source).offset();
+			},
+			move: function (e) {
+				var left = (e.pageX - offset.left),
+					top = (e.pageY - offset.top);
+
+				top = Math.max(Math.min(top, outerHeight), 0);
+				left = Math.max(Math.min(left, outerWidth), 0);
+
+				img.style.left = (left * -xRatio) + 'px';
+				img.style.top = (top * -yRatio) + 'px';
+			}
+		};
+	};
+
 	$.fn.zoom = function (options) {
 		return this.each(function () {
 			var
 			settings = $.extend({}, defaults, options || {}),
 			//target will display the zoomed iamge
 			target = settings.target || this,
-			$target = $(target),
 			//source will provide zoom location info (thumbnail)
 			source = this,
-			$source = $(source),
 			img = new Image(),
 			$img = $(img),
-			$icon,
-			position = $target.css('position'),
 			mousemove = 'mousemove',
 			clicked = false;
 
-			// The parent element needs positioning so that the zoomed element can be correctly positioned within.
-			$target.css({
-				position: /(absolute|fixed)/.test(position) ? position : 'relative',
-				overflow: 'hidden'
-			});
-
 			// If a url wasn't specified, look for an image element.
 			if (!settings.url) {
-				settings.url = $source.find('img').attr('src');
+				settings.url = $(source).find('img').attr('src');
 				if (!settings.url) {
 					return;
 				}
 			}
 
 			if (settings.icon) {
-				$icon = $('<div class="zoomIcon"/>').appendTo($source);
+				$('<div class="zoomIcon"/>').appendTo($(source));
 			}
 
 			img.onload = function () {
-				var
-				outerWidth,
-				outerHeight,
-				xRatio,
-				yRatio,
-				left,
-				top,
-				offset = $source.offset();
-
-				function ratio() {
-					outerWidth = $target.outerWidth();
-					outerHeight = $target.outerHeight();
-					xRatio = (img.width - outerWidth) / $source.outerWidth();
-					yRatio = (img.height - outerHeight) / $source.outerHeight();
-				}
-
-				function move(e) {
-					left = (e.pageX - offset.left);
-					top = (e.pageY - offset.top);
-
-					if (left > outerWidth) {
-						left = outerWidth;
-					} else if (left < 0) {
-						left = 0;
-					}
-
-					if (top > outerHeight) {
-						top = outerHeight;
-					} else if (top < 0) {
-						top = 0;
-					}
-
-					img.style.left = (left * -xRatio) + 'px';
-					img.style.top = (top * -yRatio) + 'px';
-
-					e.preventDefault();
-				}
+				var zoom = $.zoom(target, source, img);
 
 				function start(e) {
-					offset = $source.offset();
-					ratio();
-					move(e);
+					zoom.init();
+					zoom.move(e);
 
 					// Skip the fade-in for IE8 and lower since it chokes on fading-in
 					// and changing position based on mousemovement at the same time.
@@ -102,42 +105,26 @@
 					.fadeTo(settings.duration, 0);
 				}
 
-				$img
-				.addClass('zoomImg')
-				.css({
-					position: 'absolute',
-					top: 0,
-					left: 0,
-					opacity: 0,
-					width: img.width,
-					height: img.height,
-					border: 'none',
-					maxWidth: 'none'
-				})
-				.appendTo($target);
-
 				if (settings.on === 'grab') {
-					$source.mousedown(
+					$(source).mousedown(
 						function (e) {
-							offset = $source.offset();
-
 							$(document).one('mouseup',
 								function () {
 									stop();
 
-									$(document).unbind(mousemove, move);
+									$(document).unbind(mousemove, zoom.move);
 								}
 							);
 
 							start(e);
 
-							$(document)[mousemove](move);
+							$(document)[mousemove](zoom.move);
 
 							e.preventDefault();
 						}
 					);
 				} else if (settings.on === 'click') {
-					$source.click(
+					$(source).click(
 						function (e) {
 							if (clicked) {
 								// bubble the event up to the document to trigger the unbind.
@@ -147,13 +134,13 @@
 
 								start(e);
 
-								$(document)[mousemove](move);
+								$(document)[mousemove](zoom.move);
 
 								$(document).one('click',
 									function () {
 										stop();
 										clicked = false;
-										$(document).unbind(mousemove, move);
+										$(document).unbind(mousemove, zoom.move);
 									}
 								);
 
@@ -162,18 +149,17 @@
 						}
 					);
 				} else {
-					ratio(); // Pre-emptively call ratio because IE7 will fire the mousemove callback before the hover callback.
+					zoom.init(); // Pre-emptively call init because IE7 will fire the mousemove handler before the hover handler.
 
-					$source.hover(
+					$(source).hover(
 						start,
 						stop
-					)[mousemove](move);
+					)[mousemove](zoom.move);
 				}
 
 				if ($.isFunction(settings.callback)) {
 					settings.callback.call(img);
 				}
-
 			};
 
 			img.src = settings.url;
--- readme.md
+++ readme.md
@@ -4,21 +4,24 @@ A small jQuery plugin for zooming images on mouseover or mousedown. See the [pro
  
 ## Changelog:
 
-### Version 1.5 - Nov. 19 2012
+### Version 1.6.0 - 2013/1/22
+* Created $.zoom which contains the positioning logic, so that users can write custom controls or event handling.
+
+### Version 1.5.0 - 2012/11/19
 * Added 'target' property for specifying the element that displays the zoomed image.
 
-### Version 1.4 - Sept. 29 2012
+### Version 1.4.0 - 2012/9/29
 * Changed API & added option to activate on click.
 
-### Version 1.3 - Dec. 21 2011
+### Version 1.3.0 - 2011/12/21
 * Added 'callback' property that will execute a callback function once the image has loaded.
 * Fixed a bug relating to the 'grab' property
 
-### Version 1.2 - Nov. 15 2011
+### Version 1.2.0 - 2011/11/15
 * Fixed a positioning bug
 
-### Version 1.1 - Nov. 15 2011
+### Version 1.1.0 - 2011/11/15
 * Added 'grab' property
 
-### Version 1.0 - Nov. 11 2011
+### Version 1.0.0 - 2011/11/11
 * First release
