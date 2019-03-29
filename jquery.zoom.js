45b02909820c9d7d33e36ea1e07fc115e9cbace5
Heraldo Ecard Roque Filho <heraldo663@gmail.com>
2019-03-28 22:40:23 -0300

    added a factory to support webpack

--- jquery.zoom.js
+++ jquery.zoom.js
@@ -3,7 +3,17 @@
 	license: MIT
 	http://www.jacklmoore.com/zoom
 */
-(function ($) {
+; (function (factory) {
+	'use strict';
+	if (typeof define === 'function' && define.amd) {
+		define(['jquery'], factory);
+	} else if (typeof exports !== 'undefined') {
+		module.exports = factory(require('jquery'));
+	} else {
+		factory(jQuery);
+	}
+
+}(function ($) {
 	var defaults = {
 		url: false,
 		callback: false,
@@ -233,4 +243,4 @@
 	};
 
 	$.fn.zoom.defaults = defaults;
-}(window.jQuery));
+}));
