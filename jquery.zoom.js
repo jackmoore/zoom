e8a1d5dc8a5b82b7743fb35c82fc6fc20c316ac0
Gibin Ealias <gibinealias@gmail.com>
2019-08-20 14:09:16 +0530

    PR changes
    1) Avoided global apply of keyboard events. It's now only applied when the zoom is active and destroyed once the zoom is stopped.
    2) Set the keyboard navigation option as false by default, which enables the user to activate it on desired images alone.

--- jquery.zoom.js
+++ jquery.zoom.js
@@ -14,7 +14,7 @@
 		onZoomIn: false,
 		onZoomOut: false,
 		magnify: 1,
-		keyboardControls: true, //enables keyboard arrow key controls for accessibility
+		keyboardControls: false //enables keyboard arrow key controls for accessibility when set to true
 	};
 
 	// Core Zoom Logic, independent of event listeners.
@@ -121,7 +121,10 @@
 				function start(e) {
 					zoom.init();
 					zoom.move(e);
-
+					//Initialize keyboard events only when zoom is active
+					if(settings.keyboardControls) {
+						initKeyboardEvents();
+					}
 					// Skip the fade-in for IE8 and lower since it chokes on fading-in
 					// and changing position based on mousemovement at the same time.
 					$img.stop()
@@ -129,6 +132,10 @@
 				}
 
 				function stop() {
+					//Destroy keyboard events when zoom is inactive
+					if(settings.keyboardControls) {
+						$(document).off('keydown');
+					}
 					$img.stop()
 					.fadeTo(settings.duration, 0, $.isFunction(settings.onZoomOut) ? settings.onZoomOut.call(img) : false);
 				}
@@ -223,7 +230,7 @@
 				}
 				
         // Keyboard arrow key events
-				if(settings.keyboardControls) {
+				function initKeyboardEvents(){
 					$(document).on('keydown', function(e) {
 						var moveThresohld = $img.width()/10; //Setting the threshold of movement to one tenth the image width as a standard
 						switch (e.which) {
